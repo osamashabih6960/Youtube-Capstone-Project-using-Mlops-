6136dfcc50ef8d35bc036bb0c15c22132512e26c
shabihosama@gmail.com <shab>
2026-03-11 15:23:45 +0530

    local

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    packages=find_packages(),
+    version="0.1.0",
+    description="This is an end to end mlops proj",
+    author="Osama Shabih",
+    author_email="osamashabih.jamia@gmail.com",
+    licence='MIT',
+)
+
